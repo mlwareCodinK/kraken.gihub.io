0e2a319e3c76f800f76d900012d6ac891e971ace
mlwareCodinK <154976892+mlwareCodinK@users.noreply.github.com>
2024-06-15 14:13:53 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ let tg = window.Telegram.WebApp
 
 tg.expand();
 
-
+let gghh
 
 
 
@@ -218,10 +218,9 @@ element.clicker.onclick = function() {
 
 let usercard = document.getElementById("usercard"); //получаем блок usercard 
 
+gghh = `${tg.id}`
+usercard.innerText = gghh
 
-let userid = document.createElement('p'); //создаем еще параграф 
-userid.innerText = `${tg.initDataUnsafe.user.id}`; //показываем user_id
-usercard.appendChild(userid); //добавляем
 
 
 
